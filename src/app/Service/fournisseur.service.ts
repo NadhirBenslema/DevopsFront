b27752f810a4b73b4f0f041b8b56b6b819601cb1
Nadhir_bs <nadhir.benslema@esprit.tn>
2024-01-15 11:54:32 +0100

    up port service

--- src/app/Service/fournisseur.service.ts
+++ src/app/Service/fournisseur.service.ts
@@ -13,7 +13,7 @@ export class FournisseurService {
   constructor( private http : HttpClient) { }
 
   //URL du Backend
-  url = "http://192.168.1.6:8089/SpringMVC/fournisseur";
+  url = "http://192.168.1.81:8089/SpringMVC/fournisseur";
   endPoint_a="/retrieve-all-fournisseurs";
 
   getAllFournisseurs(){
